d32a263ef20b8e050a8f6692675a29a38e49b449
Zihan (Sumner) Feng <37652167+baoatwork@users.noreply.github.com>
2020-03-03 12:18:43 +0800

    lab for week3
    including text, slider and poster

---
+++ week3/lab/act1/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(800, 400);
+   }
+    
+   function draw() {
+     background(12,72,66);
+     fill('white');
+     textAlign(CENTER);
+     textFont("Helvetica");
+     textSize(20);
+     text("THE RAOD NOT TAKEN", 400, 30);
+     textAlign(LEFT);
+     textSize(16);
+     text("Two roads diverged in a yellow wood,", 10, 100);
+     text("And sorry I could not travel both", 10, 130);
+     text("And be one traveler, long I stood", 10, 160);
+     text("And looked down one as far as I could", 10, 190);
+     text("To where it bent in the undergrowth.", 10, 220);
+     
+     
+   }
