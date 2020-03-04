5e2168f28429ead0b99429154eb4b366f64cda58
Zihan (Sumner) Feng <37652167+baoatwork@users.noreply.github.com>
2020-03-04 23:33:22 +0800

    poster
    the poster assignment!

---
+++ week3/poster/sketch.js
@@ -0,0 +1,56 @@
+function preload(){
+    myFont = loadFont("dancingFont.ttf");
+    
+
+}
+
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    img = loadImage ("stardust.png");
+}
+
+function draw(){
+    //The poster background
+    fill(28,28,28);
+    rect(100,50,300,500);
+    
+    fill(255,255,251);
+    noStroke();
+
+    push();
+    translate(250,420);
+    rotate(PI/3.65);
+    rect(0,-220,1,420);
+    pop();
+
+    push();
+    translate(250,420);
+    rotate(PI/40);
+    rect(-150,-60,300,1);
+    rotate(PI /60);
+    rect(-150,60,300,1);
+   
+
+    rotate(-PI /120);
+    
+    fill(255,255,251);
+    rect(-150,1,300,1);
+    fill(129,199,212);
+    ellipse(0,0,60,36);
+    
+    pop();
+
+   
+
+    
+    textFont(myFont);
+    textSize(30);
+    text("GO", 230, 110);
+
+
+    push();
+    translate(100,50);
+    image(img,0,0,300,300);
+    pop();
+}
